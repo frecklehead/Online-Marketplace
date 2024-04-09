5b4190e0b04e20aea14afdece1c14b5a21fe46d7
frecklehead <prashamsa35np@gmail.com>
2024-04-09 18:36:13 +0545

    1

--- item/models.py
+++ item/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+#defining a model named Category inherited form models.Model with name field (charfield with max length of 255)
+class Category(models.Model):
+  name=models.CharField(max_length=255)
