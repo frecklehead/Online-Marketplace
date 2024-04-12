1f3dd19b8b7b02a107715ca5b16f64d273e4198c
frecklehead <prashamsa35np@gmail.com>
2024-04-12 14:05:29 +0545

    1

--- puddle/urls.py
+++ puddle/urls.py
@@ -3,12 +3,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path,include
-from core.views import index,contact
+
 
 
 urlpatterns = [
-  path('contact/',contact,name='contact'),
+
   path('items/',include('item.urls')),
-  path('',index,name='index'),
-    path('admin/', admin.site.urls),
-]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+  path('',include('core.urls')),
+  path('admin/', admin.site.urls),
+] +static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
