31831f75297c4467f3c4217573079f6dea561d2e
frecklehead <prashamsa35np@gmail.com>
2024-04-14 10:57:17 +0545

    1

--- core/urls.py
+++ core/urls.py
@@ -4,11 +4,13 @@ from . import views
 from .forms import LoginForm
 
 
+
+
 app_name='core'
 urlpatterns=[
   path('',views.index,name='index'),
   path('contact/',views.contact,name='contact'),
   path('signup/',views.signup,name='signup'),
    path('login/', auth_views.LoginView.as_view(template_name='core/login.html', authentication_form=LoginForm), name='login'),
-   path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'), 
+   path('logout/', auth_views.LogoutView.as_view(), name='logout'), 
 ]
