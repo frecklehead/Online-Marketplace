8223d5f893b3eb2639bf8610969ba1899875a73a
frecklehead <prashamsa35np@gmail.com>
2024-04-16 23:13:54 +0545

    1

--- item/views.py
+++ item/views.py
@@ -1,13 +1,31 @@
 from django.shortcuts import render,get_object_or_404,redirect
 from django.contrib.auth.decorators import login_required
-from .models import Item
+from .models import Item,Category
+from django.db.models import Q
 from .forms import NewItemForm,EditItemForm
 
+
 def items(request):
+   query=request.GET.get('query','')
+   categories=Category.objects.all()
+   category_id=request.GET.get('category',0)
+   if category_id:
+      items=items.filter(category_id=category_id)
+   
+ 
    items=Item.objects.filter(is_sold=False)
+   if query:
+      items=items.filter(Q(name__icontains=query)|Q(description__icontains=query))
+     
+      
    return render(request,'item/items.html',{
       'items':  items,
-   })
+      'query': query,
+      'categories':categories,
+      'category_id':int(category_id),
+
+      
+      })
 
 # Create your views here.
 def detail(request,pk):
@@ -16,6 +34,7 @@ def detail(request,pk):
   return render(request,'item/detail.html',{
     'item':item,
     'related_items':related_items
+
 
   })
 @login_required
